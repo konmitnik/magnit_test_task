328c454edd54fadfc039df96003399750417e742
konmitnik <konmitnik@gmail.com>
2022-04-03 22:58:11 +0300

    add pdf parser function

--- pdf.py
+++ pdf.py
@@ -1,11 +1,28 @@
-from database import getUsers
+from database import insertUser, getUsers
 from sqlite3 import Connection
 from fpdf import FPDF
+import pdfplumber
 
 
 def importFromFile(path: str, conn: Connection):
     """Import data about user from PDF file to DB"""
-    pass
+
+    try:
+        with pdfplumber.open(path) as pdf:
+            text = pdf.pages[0].extract_text()
+            text = text.split(' \n')
+            user = []
+            user.extend(text[2].split(' '))
+            user.extend(text[4].split(', '))
+            user.extend((text[5], text[6]))
+
+            insertUser(user, conn)
+            conn.commit()
+
+            print('Success!')
+    except FileNotFoundError as e:
+        print(e)
+        return
 
 
 def exportToFile(filename, conn):
